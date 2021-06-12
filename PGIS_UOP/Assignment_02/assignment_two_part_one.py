8da1f806ba6504773372990abf5061e74225298b
lalithk90 <asakahatapitiya@gmail.com>
2021-06-12 20:54:31 +0530

    sample code was updated

--- PGIS_UOP/Assignment_02/assignment_two_part_one.py
+++ PGIS_UOP/Assignment_02/assignment_two_part_one.py
@@ -5,24 +5,25 @@ problemSizes = [1000, 2000, 3000, 4000, 10000]
 
 
 def accordingProblemSize(value):
-print("%12s%15s" % ("Problem Size", "Iterations"))
-start_time = time.time()
-problemSize = value
-while problemSize > 0:
-number = 0
-for j in range(problemSize):
-for k in range(problemSize):
-number += 1
-print("%12d%15d" % (problemSize, number))
-problemSize = problemSize // 2
-end_time = time.time()
-total_time = (end_time - start_time)
-return total_time
+    global total_time
+    print("%12s%15s" % ("Problem Size", "Iterations"))
+    start_time = time.time()
+    problem_size = value
+    while problem_size > 0:
+        number = 0
+        for j in range(problem_size):
+            for k in range(problem_size):
+                number += 1
+                print("%12d%15d" % (problem_size, number))
+                problem_size = problem_size // 2
+        end_time = time.time()
+        total_time = (end_time - start_time)
+    return total_time
 
 
 finalResult = []
 for x in problemSizes:
-finalResult.append("Problem Size : " + str(x) + " Total Time : " + str(accordingProblemSize(x))+"s")
+    finalResult.append("Problem Size : " + str(x) + " Total Time : " + str(accordingProblemSize(x)) + "s")
 
 for y in finalResult:
-print(y)
+    print(y)
